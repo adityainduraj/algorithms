ea2ae5f4c6846ed3cfe194d17b6d338f1a4710d6
adityainduraj <adityainduraj.dev@gmail.com>
2024-04-17 10:36:58 +0530

    added dijkstras

--- lab8/top-down-heap-creation.c
+++ lab8/top-down-heap-creation.c
@@ -3,15 +3,28 @@
 #include <time.h>
 
 void topDownHeapify(int arr[], int n, int i) {
-    // Heapify implementation...
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        topDownHeapify(arr, n, largest);
+    }
 }
 
 void buildHeap(int arr[], int n) {
-    // buildHeap implementation...
-}
-
-void printHeap(int arr[], int n) {
-    // printHeap implementation...
+    for (int i = n / 2 - 1; i >= 0; i--)
+        topDownHeapify(arr, n, i);
 }
 
 int main() {
@@ -19,25 +32,24 @@ int main() {
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
-    int *arr = (int *)malloc(n * sizeof(int));
-
+    int arr[n];
     printf("Enter the elements:\n");
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
-    clock_t start = clock(); // Start the clock
+    clock_t start = clock(); // Stop the clock
 
     buildHeap(arr, n);
 
     clock_t end = clock(); // Stop the clock
 
-    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC; // Cast the result to double
+    
     printf("Heap construction time: %f seconds\n", time_taken);
 
-    printHeap(arr, n);
-
-    free(arr);
+    printf("Heap array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
 
     return 0;
 }
